5d55feb3ec02cbc4660c13de631d7cabd544f164
Axel <a.i.cpvd3000@gmail.com>
2023-01-03 22:53:14 -0300

    New version threejs

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,10 @@ export default function Home() {
 
 
   return (
-    <>
       <Head>
         <title>AXRSKF | The Nerd's Dungeon</title>
         <meta name="keywords" content="This web portfolio showcases the skills and experience of a software developer. It includes examples of past projects and technologies used. The developer is proficient in a variety of programming languages and has a strong track record of delivering high-quality software solutions. Whether you are in need of a custom application or just looking to learn more about the developer's abilities, this portfolio is a great resource."/>
         <meta name="author" content="Axo Rosskopf"/>
       </Head>
-      <script/>
-    </>
   )
 }
